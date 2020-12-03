dacbfcf558df0fcb191ecb741b52dfcaa0e88b4e
Matheus Mello <matheusmellocunha@gmail.com>
2020-12-03 20:08:58 -0300

    primeiro commit

---
+++ jogo-forca/forca.py
@@ -0,0 +1,30 @@
+def jogar():
+
+	print('\n*'*10)
+	print('***** BEM VINDO AO JOGO DA FORCA *****')
+	print('*'*10, '\n')
+	
+	# SETTING THE SECRET WORD
+	secret_word = 'banana'
+
+	# MENU OF TRIES
+	print('''Menu de Tentativas
+	(1) Fácil - 20 tentativas
+	(2) Médio - 10 tentativas
+	(3) Difícil - 5 tentivas''')
+	
+	while True:
+		try:
+			level = int(input('digite o nível do jogo: '))
+
+			if level == 1 or level == 2 or level == 3:
+				break
+			else:
+				print('Ensira um número de 1 a 3, por favor.')
+
+		except ValueError:
+			print('Somente números, por favor.')
+
+
+if __name__ == '__main__':
+	jogar()
