d66830214a98ee90d940e9afdbde38bc7769e017
Matheus Mello <matheusmellocunha@gmail.com>
2020-12-05 20:08:03 -0300

    Alterando as saídas do if

--- jogo-forca/forca.py
+++ jogo-forca/forca.py
@@ -1,8 +1,8 @@
 def jogar():
 
-	print('\n*'*10)
+	print('\n\n','*'*37)
 	print('***** BEM VINDO AO JOGO DA FORCA *****')
-	print('*'*10, '\n')
+	print('*'*38, '\n')
 	
 	# SETTING THE SECRET WORD
 	secret_word = 'banana'
@@ -10,17 +10,21 @@ def jogar():
 	# SETTING A LIST TO PUT THE USERS'S VALUES
 	answer_secret_word = []
 
-	for chances in range(level):
+	for chances in range(len(secret_word)):
 		user_answer = input('Digite uma letra: ')
 
 		if user_answer in secret_word and user_answer not in answer_secret_word:
 			for letra in secret_word:
 				if letra == user_answer:
 					answer_secret_word.append(letra)
+					print(f'Parabéns, temos a letra {letra} em {secret_word.count(letra)} posições.')
 
+
+		elif user_answer in secret_word:
+			print('\nVocê já digitou essa letra')
 
 		else:
-			print('Não tem essa letra na palavra.')
+			print(f'\nA palatra secreta não possui a letra {user_answer}')
 
 	print(answer_secret_word)
 
