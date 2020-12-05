2014b98f7db7ebf17d324cdfab389f49fcc05db7
Matheus Mello <matheusmellocunha@gmail.com>
2020-12-05 18:54:53 -0300

    Removendo o menu de tentativas

--- jogo-forca/forca.py
+++ jogo-forca/forca.py
@@ -7,35 +7,6 @@ def jogar():
 	# SETTING THE SECRET WORD
 	secret_word = 'banana'
 
-	# MENU OF TRIES
-	print('''Menu de Tentativas
-	(1) Fácil - 20 tentativas
-	(2) Médio - 10 tentativas
-	(3) Difícil - 5 tentivas''')
-	
-	while True:
-		try:
-			level = int(input('digite o nível do jogo: '))
-
-			if level == 1:
-				level = 20
-				break
-
-			elif level == 2:
-				level = 15
-				break
-
-			elif level == 3:
-				level = 5	
-				break
-
-			else:
-				print('Ensira um número de 1 a 3, por favor.')
-
-		except ValueError:
-			print('Somente números, por favor.')
-
-
 	# SETTING A LIST TO PUT THE USERS'S VALUES
 	answer_secret_word = []
 
@@ -46,6 +17,8 @@ def jogar():
 			for letra in secret_word:
 				if letra == user_answer:
 					answer_secret_word.append(letra)
+
+
 		else:
 			print('Não tem essa letra na palavra.')
 
